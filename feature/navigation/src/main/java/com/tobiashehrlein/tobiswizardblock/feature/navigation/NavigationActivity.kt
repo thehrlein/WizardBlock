6c34f43420cd7ea5dd25bd07fb075a456bfaff5e
Tobias Hehrlein <tobiashehrlein@gmail.com>
2025-10-03 13:12:57 +0200

    [FIX] navigation activity screen height, adjust toolbar size

--- feature/navigation/src/main/java/com/tobiashehrlein/tobiswizardblock/feature/navigation/NavigationActivity.kt
+++ feature/navigation/src/main/java/com/tobiashehrlein/tobiswizardblock/feature/navigation/NavigationActivity.kt
@@ -11,6 +11,7 @@ import com.tobiashehrlein.tobiswizardblock.feature.common.ui.BaseActivity
 import com.tobiashehrlein.tobiswizardblock.feature.common.utils.ScreenHelper
 import com.tobiashehrlein.tobiswizardblock.feature.common.utils.extensions.isUsingDarkMode
 import com.tobiashehrlein.tobiswizardblock.feature.navigation.databinding.ActivityNavigationBinding
+import com.tobiashehrlein.tobiswizardblock.old.utils.helper.WindowInsetsHelper
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -32,6 +33,8 @@ class NavigationActivity : BaseActivity<BaseNavigationViewModel, ActivityNavigat
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         val screenSize = ScreenHelper.getScreenSize(this)
         Timber.d("ScreenSize: $screenSize")
 
@@ -45,5 +48,21 @@ class NavigationActivity : BaseActivity<BaseNavigationViewModel, ActivityNavigat
             if (isDarkMode) com.tobiashehrlein.tobiswizardblock.feature.common.R.string.tracking_user_property_system_appearance_dark else com.tobiashehrlein.tobiswizardblock.feature.common.R.string.tracking_user_property_system_appearance_light
         )
         viewModel.trackSystemAppearanceUserProperty(systemAppearance)
+    }
+
+    override fun onBindingCreated() {
+        adjustStatusBarHeight()
+    }
+
+    private fun adjustStatusBarHeight() {
+        WindowInsetsHelper.getWindowInsets(binding.root, window) { vbInsets ->
+            binding.statusBarBackground.layoutParams.height = vbInsets.statusBarHeight
+            binding.root.setPadding(
+                binding.root.paddingLeft,
+                binding.root.paddingTop,
+                binding.root.paddingRight,
+                vbInsets.navigationBarHeight
+            )
+        }
     }
 }
